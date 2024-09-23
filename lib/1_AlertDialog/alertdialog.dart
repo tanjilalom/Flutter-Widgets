02843e33c93f5124fb2156f483dc4af6839ac026
tanjilalom <tanjil15-3079@diu.edu.bd>
2024-09-23 20:36:57 +0600

    stuff looks better

---
+++ lib/1_AlertDialog/alertdialog.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class MyAlertDialog extends StatefulWidget {
+  const MyAlertDialog({super.key});
+
+  @override
+  State<MyAlertDialog> createState() => _MyAlertDialogState();
+}
+
+class _MyAlertDialogState extends State<MyAlertDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Alert Dialog'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: MaterialButton(
+          color: Colors.deepPurple,
+          onPressed: _showDialog,
+          child: const Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              'SHOW DIALOG',
+              style: TextStyle(fontSize: 24),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showDialog() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Title'),
+          content: const Text('Content, more information'),
+          actions: [
+            MaterialButton(
+              color: Colors.deepPurple[100],
+              onPressed: () {
+                // do something
+                Navigator.pop(context);
+                print('Ok button pressed');
+              },
+              child: const Text('Ok'),
+            ),
+            MaterialButton(
+              color: Colors.deepPurple[100],
+              onPressed: () {
+                // do something
+                Navigator.pop(context);
+                print('Cancel button pressed');
+              },
+              child: const Text('Cancel'),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
